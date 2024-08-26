72c1fdbc3f76c0f76fcd580320bbaf98fcb6f5a7
hemanthxai <hemanthsai.rockz@gmail.com>
2024-08-26 11:59:10 +0530

    custom validators

--- pydanticimplementation.py
+++ pydanticimplementation.py
@@ -1,11 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, validator
 
 
 class User(BaseModel):
     id: int
     name: str
-    email: str
+    email: EmailStr
+
+    @validator("id")
+    def validate_id(cls, value):
+        if value < 0:
+            raise ValueError("ID value must be greater than 0")
+        return value
 
 
-user = User(id="0001", name="Hemanth", email="hsk@gmail.com")
+user = User(id=1, name="Hemanth", email="hsk@gmail.com")
 print(user)
