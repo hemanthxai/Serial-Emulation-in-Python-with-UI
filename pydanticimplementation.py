00bf182caef79573c6c7d8858ed7e8181b405fc2
hemanthxai <hemanthsai.rockz@gmail.com>
2024-08-26 12:22:13 +0530

    in built validators in pydantic

--- pydanticimplementation.py
+++ pydanticimplementation.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel, EmailStr, validator
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    AnyUrl,
+    PositiveInt,
+    validator,
+)
 
 
 class User(BaseModel):
@@ -15,3 +21,36 @@ class User(BaseModel):
 
 user = User(id=1, name="Hemanth", email="hsk@gmail.com")
 print(user)
+
+
+# Using inbuilt Validators in Pydantic
+
+
+class UserProfile(BaseModel):
+
+    username: str
+
+    age: PositiveInt
+
+    salary: PositiveInt
+
+    email: EmailStr
+
+    gitwebsite: AnyUrl
+
+    bio: str
+
+    profile_picture: AnyUrl
+
+
+newuser = UserProfile(
+    username="Hemanth",
+    age=21,
+    salary=1000,
+    debt=0.0,
+    email="user@example.com",
+    gitwebsite="https://e.com",
+    bio="IoT Python developer",
+    profile_picture="https://github.com/hemanthxai/TechnoCulture-Research/",
+)
+print(newuser)
