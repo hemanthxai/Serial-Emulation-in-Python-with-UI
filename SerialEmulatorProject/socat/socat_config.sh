f72bd7a0fe6bf2ac49b875f054ef8b9d25a84c72
Hemanth Sai Krishna <139855530+hemanthxai@users.noreply.github.com>
2024-08-30 12:53:45 +0530

    Delete SerialEmulatorProject directory

--- SerialEmulatorProject/socat/socat_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Create virtual serial ports
-socat -d -d pty,link=/dev/ttyS1,raw,echo=0 pty,link=/dev/ttyS2,raw,echo=0 &
-socat -d -d pty,link=/dev/ttyS3,raw,echo=0 pty,link=/dev/ttyS4,raw,echo=0 &
