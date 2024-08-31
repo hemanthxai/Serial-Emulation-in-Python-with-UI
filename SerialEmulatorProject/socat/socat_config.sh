9068a17211407f83d22bd5bc62f43d9fcdeed8e5
Hemanth Sai Krishna <139855530+hemanthxai@users.noreply.github.com>
2024-08-31 18:46:27 +0530

    Delete SerialEmulatorProject directory

--- SerialEmulatorProject/socat/socat_config.sh
+++
@@ -1,3 +0,0 @@
-sudo socat -d -d PTY,link=/dev/ttyS1,raw,echo=0 PTY,link=/dev/ttyS2,raw,echo=0 &
-sudo socat -d -d PTY,link=/dev/ttyS3,raw,echo=0 PTY,link=/dev/ttyS4,raw,echo=0 &
-sudo socat -d -d PTY,link=/dev/ttyS5,raw,echo=0 PTY,link=/dev/ttyS6,raw,echo=0 &
